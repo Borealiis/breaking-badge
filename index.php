6c01aac31326b8b7cd82b99607d7d79953fac3db
bruxellesflorian <florianbertchi@gmail.com>
2021-01-19 14:18:55 +0100

    pageLogOver

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
-<?php
+<!-- <?php
   session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/style.css">
-  <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-
-  <title>Breaking Badge</title>
-</head>
-<body>
-  <!-- <?php include_once('components/router.php'); ?> -->
-  <div></div>
-  <form action="" method="post">
-  <input type="text" placeholder="Email">
-  <input type="text" placeholder="Password">
-  </form>
-
-</body>
-</html>
+  ?> -->
+  
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="assets/style.css">
+      <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+  
+      <title>Breaking Badge</title>
+  </head>
+  
+  <body>
+      <!-- <?php include_once('components/router.php'); ?> -->
+      <div class="middleCenter">
+          <div class="flexLogPage">
+              <div class="flexLock"><i class="fas fa-unlock fa-9x"></i></div>
+              <form action="" method="post">
+              <div><input class="sizeInput"type="text" placeholder="Email"></div>
+              <div><input class="sizeInput"type="text" placeholder="Password"></div>
+              <div><input class="sizeInput"type="button" value="Submit"></div>
+          </form>
+      </div>
+  </div>
+  </div>
+  
+  </body>
+  
+  </html>
