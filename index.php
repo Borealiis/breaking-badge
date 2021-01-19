ae8a548256841eeb40a972b5ce28ddb9ed9b4936
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-01-19 15:48:19 +0100

    add include admin_dashboard

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<!-- <?php
+<?php
   session_start();
-  ?> -->
-  
-  <!DOCTYPE html>
-  <html lang="en">
-  
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="assets/style.css">
-      <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
-  
-      <title>Breaking Badge</title>
-  </head>
-  
-  <body>
-      <!-- <?php include_once('components/router.php'); ?> -->
-            <?php include('pages\login.php'); ?>
-  
-  </body>
-  
-  </html>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+
+    <title>Breaking Badge</title>
+</head>
+
+<body>
+<!-- <?php include_once('components/router.php'); ?> -->
+<!--      <?php //include('pages\login.php'); ?>-->
+      <?php include('pages\dashboard_admin.php'); ?>
+
+</body>
+</html>
