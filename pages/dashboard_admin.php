3e9d08f756cfffe3260e03c40581b28cc70d89f8
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-01-19 15:48:58 +0100

    first draft for dashboard_admin.php

---
+++ pages/dashboard_admin.php
@@ -0,0 +1,50 @@
+<!--TODO: (Aurore) remplacer variables entre crochets par variables php -->
+
+<main>
+    <section class="sidebar">
+        <div class="welcome">Welcome, [USER]</div>
+        <div class="sidebarBadgeInfo">
+            <div class="badgeNotif">
+                <p>No news... Yet</p>
+            </div>
+            <div class="myBadges"></div>
+            <div class="badgesToGet"></div>
+            <div class="myBadgesProgress"></div>
+        </div>
+    </section>
+
+    <div class="userInfo">
+        <section class="userHeader">
+<!--            <img src="" alt="" class="userImage">-->
+            <p>[IMAGE]</p>
+            <p>USERNAME</p>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam corporis culpa possimus quod! Adipisci autem id inventore non perferendis quod repudiandae sint totam. Necessitatibus numquam obcaecati omnis ut voluptatum.</p>
+        </section>
+        <section class="badgesAndUsers">
+            <div class="viewUsers">
+                <button class="seeAllUsers">All users</button>
+                <div class="mostRecentUsers">
+                    <p class="subtitle">Last users</p>
+                    <ul>
+                        <li>USER1</li>
+                        <li>USER2</li>
+                        <li>USER3</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="viewBadges">
+                <button class="seelAllBadges">All Badges</button>
+                <button class="addBadge">+</button>
+                <div class="recentBadges">
+                    <p class="subtitle">Last badges</p>
+
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <section class="otherUsersDo">
+
+    </section>
+
+</main>
