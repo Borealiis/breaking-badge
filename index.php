d0af009afbeaecd088515a323b669220d548e663
bruxellesflorian <florianbertchi@gmail.com>
2021-01-19 14:22:07 +0100

    modif

--- index.php
+++ index.php
@@ -17,17 +17,7 @@
   
   <body>
       <!-- <?php include_once('components/router.php'); ?> -->
-      <div class="middleCenter">
-          <div class="flexLogPage">
-              <div class="flexLock"><i class="fas fa-unlock fa-9x"></i></div>
-              <form action="" method="post">
-              <div><input class="sizeInput"type="text" placeholder="Email"></div>
-              <div><input class="sizeInput"type="text" placeholder="Password"></div>
-              <div><input class="sizeInput"type="button" value="Submit"></div>
-          </form>
-      </div>
-  </div>
-  </div>
+            <?php include('pages\login.php') ?>
   
   </body>
   
