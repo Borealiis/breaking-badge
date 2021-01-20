23b60325c1c89e9865e4a5faab4005f637d3d028
Guy Richard Ibambasi <72995913+GuyRichardib@users.noreply.github.com>
2021-01-20 14:26:50 +0100

    second commit

---
+++ components/db.php
@@ -0,0 +1,13 @@
+
+
+<?php
+
+  function createCursor(){
+    $DB_HOST = 'localhost';
+    $DB_NAME = 'breaking_badge';
+    $DB_USER = 'user';
+    $DB_PASSWORD = 'P@ssW0rd';
+      return new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8",$DB_USER,$DB_PASSWORD,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+      );
+  }
+?>
