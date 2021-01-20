1ad98268075bf29871a3919fda29d1f53dd3c981
Audrey <73021969+GAudrey@users.noreply.github.com>
2021-01-20 14:35:59 +0100

    add allBadges page and function getAllBadges

--- components/functions.php
+++ components/functions.php
@@ -89,6 +89,16 @@ function signin(){
 
   }
 
+  function getAllBadges(){
+    $cursor = createCursor();
+    $recherche = $cursor->query("SELECT * FROM table_badges");
+    while($donnee = $recherche->fetch())
+    {
+      echo $donnee['badge_name'];
+    }
+    $recherche->closeCursor();
+  }
+
   function getUsers(){
 
   }
