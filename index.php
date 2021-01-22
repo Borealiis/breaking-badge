166d478ea62c5c9df70f77d3af89cf3c68f7d759
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-01-22 16:08:28 +0100

    change icon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="assets/style.css">
-      <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+      <link rel="shortcut icon" href="assets/image/icon.png" type="image/x-icon">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&family=Chakra+Petch&display=swap" rel="stylesheet">
