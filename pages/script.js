cf4cdfaed848848062e5a33f76aded31b331630b
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-01-22 09:52:25 +0100

    deleted for loop line 46 + bracket l 51

--- pages/script.js
+++ pages/script.js
@@ -43,9 +43,8 @@ const userLIist = document.querySelector('.userslist');
 
 const progress = document.querySelector('.progress-done');
 
-for(var i = 0; i < 10; i++){
+
 setTimeout(() => {
     progress.style.width = progress.getAttribute('data-done') + '%';
     progress.style.opacity = 1;
 }, 500)
-};
